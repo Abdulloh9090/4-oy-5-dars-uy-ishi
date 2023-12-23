5dd058daedb1f9b925919fc6498e29cfdcb1d6a8
Abdulloh9090 <152011956+Abdulloh9090@users.noreply.github.com>
2023-12-23 16:50:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let son = prompt ("bir son kiriting")
+let son2 = prompt ("Yana bir son kiriting")
+son ++
+son2 --
+console.log("Siz kiritgan 1-son " + son + " ga teng edi, " + son + " ga o'zgartirildi");
+
+
+
+
+
+console.log("Siz kiritgan 2-son " + son2 + " ga teng edi, " + son2 + " ga o'zgartirildi");
